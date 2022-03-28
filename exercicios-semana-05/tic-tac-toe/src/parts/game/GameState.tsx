21f8127e0e7d9a02139115c291a83f2b7590b0a4
elielbianchi <elielbianchi@hotmail.com>
2022-03-28 00:14:36 -0300

    Created the game logic and placed on file GameState

---
+++ exercicios-semana-05/tic-tac-toe/src/parts/game/GameState.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+export type SquareValue = "X" | "O" | "";
+
+export type BoardState = SquareValue[];
+
+const createBoard = () => Array<SquareValue>(9).fill("");
+
+function calculateWinner(boardState: BoardState) {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+
+    if (
+      boardState[a] &&
+      boardState[a] === boardState[b] &&
+      boardState[a] === boardState[c]
+    ) {
+      return boardState[a];
+    }
+  }
+  return null;
+}
+
+export type GameState = {
+  played: BoardState[];
+  round: number;
+};
+
+export function useGameState() {
+  const [gameState, setGameState] = useState<GameState>({
+    played: [createBoard()],
+    round: 0,
+  });
+
+  const current = gameState.played[gameState.round];
+  const xIsNext = gameState.round % 2 === 0;
+  const winner = calculateWinner(current);
+
+  function handleClick(square: number) {
+    const played = gameState.played.slice(0, gameState.round + 1);
+    const current = played[played.length - 1];
+    const squares = [...current];
+    if (winner || squares[square]) {
+      return;
+    }
+    squares[square] = xIsNext ? "X" : "O";
+    setGameState({
+      played: played.concat([squares]),
+      round: played.length,
+    });
+  }
+
+  return {
+    gameState,
+    current,
+    xIsNext,
+    winner,
+    handleClick,
+  };
+}
